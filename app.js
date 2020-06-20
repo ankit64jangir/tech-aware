b2a6f1c42fdb5de1d4319834743efa2800e15ed5
Ankit Jangir <ankit2000jangir@gmail.com>
2020-06-20 15:07:07 +0530

    Successfully added registration and login system

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const flash = require('connect-flash');
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const session = require('express-session');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
 
+// Passport Config
+require('./config/passport')(passport);
 
 // DB Config
 const db = require('./config/keys').mongoURI;
@@ -35,6 +42,18 @@ app.use(express.urlencoded({
 }));
 
 
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+
+
+
 
 // Connect flash
 app.use(flash());
@@ -59,10 +78,82 @@ app.get("/", function(req, res) {
 // Login Page
 app.get('/login', (req, res) => res.render('login'));
 
-
-
 // Register Page
 app.get('/register', (req, res) => res.render('register'));
+
+// Register
+app.post('/register', (req, res) => {
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: 'Please enter all fields' });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    User.findOne({ email: email }).then(user => {
+      if (user) {
+        errors.push({ msg: 'Email already exists' });
+        res.render('register', {
+          errors,
+          name,
+          email,
+          password,
+          password2
+        });
+      } else {
+        const newUser = new User({
+          name,
+          email,
+          password
+        });
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  'success_msg',
+                  'You are now registered and can log in'
+                );
+                res.redirect('/login');
+              })
+              .catch(err => console.log(err));
+          });
+        });
+      }
+    });
+  }
+});
+
+// Login
+app.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
 
 
 let port = process.env.PORT;
