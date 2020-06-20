6fef0449fd4804db01f488b1a84f7708fe0c757f
Ankit Jangir <ankit2000jangir@gmail.com>
2020-06-20 13:32:05 +0530

    Added welcome ejs template and setup public folder

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
 
 
 
 
 const app = express();
+app.use(express.static(__dirname + '/public'));
+
+
+
+// EJS
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
 
 
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  res.render("welcome");
 });
 
 
